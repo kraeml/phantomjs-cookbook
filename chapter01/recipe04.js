85c1a446b64419b5c02c8ad58f294182f50e5ba1
kraeml <kraeml@sesamestreet.franken.de>
2017-08-27 13:22:52 +0200

    recipe04.js with system

--- chapter01/recipe04.js
+++ chapter01/recipe04.js
@@ -4,10 +4,12 @@
  * Chapter 1 | Getting Started with PhantomJS
  * Recipe 4  | Running a PhantomJS script with arguments
  */
-if (phantom.args.length === 0) {
+var system = require('system');
+
+if (system.args.length === 0) {
   console.log('No arguments were passed in.');
 } else {
-  phantom.args.forEach(function(arg, index) {
+  system.args.forEach(function(arg, index) {
     console.log('[' + index + '] ' + arg);
   });
 }
